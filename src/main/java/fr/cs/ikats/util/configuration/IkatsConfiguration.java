54d3d1b4c45f96c408d36292150126f7385d04aa
ftoral <fabien.toral@c-s.fr>
2016-10-14 18:41:29 +0200

    [#147170] Move of config into ikats-commons

--- src/main/java/fr/cs/ikats/util/configuration/IkatsConfiguration.java
+++
@@ -1,9 +0,0 @@
-package fr.cs.ikats.util.configuration;
-
-public class IkatsConfiguration<T extends Enum<T> & ConfigProperties> extends AbstractIkatsConfiguration<T> {
-
-	public IkatsConfiguration(Class<T> configPropertiesClazz) {
-		super.init(configPropertiesClazz);
-	}
-
-}
